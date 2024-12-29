03793c8937ed1174ef66180acb6812ab97350776
EthanVletter <ethan.vletter@gmail.com>
2024-12-29 21:39:07 +0200

    Intial js files and updates to html and css

---
+++ js/index.js
@@ -0,0 +1,39 @@
+function homeToggle() {
+    var x = document.getElementById("introduction-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function aboutmeToggle() {
+    var x = document.getElementById("aboutme-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function projectsToggle() {
+    var x = document.getElementById("projects-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function contactinfoToggle() {
+    var x = document.getElementById("contactinfo-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
