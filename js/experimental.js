09db496ccddb653d6f27f0270bcf99e28d7bb935
EthanVletter <ethan.vletter@gmail.com>
2025-01-10 21:31:42 +0200

    Added experimental setting

---
+++ js/experimental.js
@@ -0,0 +1,44 @@
+function homeToggle() {
+    var x = document.getElementById("home-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function aboutmeToggle() {
+    var x = document.getElementById("aboutme-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function projectsToggle() {
+    var x = document.getElementById("projects-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function contactinfoToggle() {
+    var x = document.getElementById("contactinfo-block");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function copyDiscord() {
+    navigator.clipboard.writeText("LTSanic#7244");
+    alert("Copied to clipboard!");
+}
