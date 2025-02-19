3199fa2faa6a7c7dea2f0c75dff8b80cb7d1210b
EthanVletter <ethan.vletter@gmail.com>
2025-02-19 21:22:59 +0200

    added scrolling animations

--- js/index.js
+++ js/index.js
@@ -42,15 +42,24 @@ function flipCard(card) {
     card.classList.toggle("flipped");
 }
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.add("remove");
-        }
-    });
-});
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                // entry.target.classList.remove("hidden");
+            }
+            // else {
+            // entry.target.classList.add("hidden");
+            // entry.target.classList.remove("show");
+            // }
+        });
+    },
+    {
+        // threshold: 0.2,
+    }
+);
 
-const hiddenElements = document.querySelectorAll("hidden");
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
